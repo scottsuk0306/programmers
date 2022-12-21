cf64681aea6bd1e8a6bb13ba31a298d6e9eed414
judys0303 <judys0303@gmail.com>
2022-12-21 23:40:47 +0900

    Update N개의-최소공배수.py

--- level-2/N개의-최소공배수.py
+++ level-2/N개의-최소공배수.py
@@ -13,7 +13,7 @@ def solution(arr):
     #두 개의 수끼리 최소공배수를 구하고 최소공배수와 다음 수의 최소공배수 구하기
     for i in range(1, len(arr)):
         gcd_num = gcd(arr[i-1], arr[i])
-        answer = int(arr[i-1] * arr[i] / gcd_num)
+        answer = arr[i-1] * arr[i] // gcd_num
         arr[i] = answer
 
     return answer
