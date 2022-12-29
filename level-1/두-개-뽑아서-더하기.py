5ff101361bcf9ec52468ae949dacfb7cdcde7715
judys0303 <judys0303@gmail.com>
2022-12-29 22:01:18 +0900

    Update 두-개-뽑아서-더하기.py

--- level-1/두-개-뽑아서-더하기.py
+++ level-1/두-개-뽑아서-더하기.py
@@ -2,8 +2,8 @@ def solution(numbers):
     answer = []
     for i in range(len(numbers)):
         for j in range(i+1, len(numbers)):
-            if numbers[i]+numbers[j] not in answer:
-                answer.append(numbers[i] + numbers[j])
+            answer.append(numbers[i] + numbers[j])
     
-    answer.sort()
-    return answer
+    answer = list(set(answer))
+    
+    return sorted(answer)
